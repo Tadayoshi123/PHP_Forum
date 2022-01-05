d467f6d8854629f3f9a81bed79cfbc333f7cf6cd
Tadayoshi123 <yankill@outlook.fr>
2022-01-05 17:08:30 +0100

    First commit

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet Forum</title>
-    <link rel="icon" href="/static/img/icon/forum.png">
-    <!-- fontstyle -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Montserrat+Alternates:wght@300&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/static/css/index.css">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP_Forum</title>
+	<style>
+		.sidenav {
+			display: flex;
+			height: 10%;
+			width: 100%;
+			position: fixed;
+			top: 0;
+			left: 0;
+			background-color: #111;
+			padding-top: 20px;
+		}
+
+		.sidenav a {
+			padding: 6px 8px 6px 16px;
+			text-decoration: none;
+			font-size: 25px;
+			color: #818181;
+
+		}
+
+		.sidenav a:hover {
+			color: #f1f1f1;
+		}
+	</style>
 </head>
 
 <body>
-    <header>
-        <nav class="firstblock">
-            <div class="forum_title">
-                <!-- <div class="leftnav">
-                    <img src="/static/img/icon/forum.png" alt="ForumIcon">
-                    <a href="/index">Forum</a>
-                </div> -->
-                <div class="rightnav">
-                    <div id="menu">
-                        <!-- <a class="UserName" id="UserName" href="/user"><img src="/static/img/icon/usericon.png" alt="iconuser"> {{.User.Username}}</a> -->
-                        <a href="/php_forum/login.php" id="Connexion" class="Connexion">Connexion</a>
-                        <a href="/php_forum/register.php" id="Inscription" class="Inscription">Inscription</a>
-                        <form method="post">
-                            <button class="Deconnexion" id="Deconnexion" name="Deconnexion" value="run">Deconnexion</a>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+	<div class="sidenav">
+		<a href="/PHP_Forum/php_exam/login.php">Login</a>
+		<a href="/PHP_Forum/php_exam/register.php">Register</a>
+		<a href="/PHP_Forum/php_exam/home.php">Home</a>
+		<a href="/PHP_Forum/php_exam/new.php">New</a>
+		<a href="/PHP_Forum/php_exam/details.php">Details</a>
+		<a href="/PHP_Forum/php_exam/edit.php">Edit</a>
+		<a href="/PHP_Forum/php_exam/account.php">Account</a>
+		<a href="/PHP_Forum/php_exam/connexion_admin.php">Connexion Admin</a>
+		<a href="/PHP_Forum/php_exam/panel_admin.php">Panel Admin</a>
+	</div>
 </body>
+
+</html>
