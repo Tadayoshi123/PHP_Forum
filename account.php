574fb9dbe13238f7583097668ca0abc1039a20b4
Tadayoshi123 <yankill@outlook.fr>
2022-01-23 18:36:12 +0100

    still more front

--- account.php
+++ account.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account</title>
+    <title>Mon Compte</title>
     <style>
         <?php include 'static/css/account.css'; ?>
     </style>
